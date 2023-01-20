6a0f8a74e7d67614c4848e3cb28c698f88d73440
Nikita Grishin <ng222jk@student.lnu.se>
2023-01-20 18:38:37 +0100

    more responsive css

--- src/js/modules/time.js
+++ src/js/modules/time.js
@@ -1,66 +1,61 @@
 function getTimeDifference(currentTime, lastBroadcast) {
-  let timeDifference = currentTime - lastBroadcast
-  let days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
-  let hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
-  let minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
-  let seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
+  if (lastBroadcast === undefined) {
+    return {
+      days: ".",
+      hours: ".",
+      minutes: ".",
+      seconds: ".",
+    }
+  }
+
+  const timeDifference = currentTime - lastBroadcast
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000)
 
   return {
-    days: Math.abs(days).toString(),
-    hours: Math.abs(hours).toString().padStart(2, "0"),
-    minutes: Math.abs(minutes).toString().padStart(2, "0"),
-    seconds: Math.abs(seconds).toString().padStart(2, "0"),
+    days: days.toString(),
+    hours: hours.toString().padStart(2, "0"),
+    minutes: minutes.toString().padStart(2, "0"),
+    seconds: seconds.toString().padStart(2, "0"),
   }
 }
 
-// Function that updates the timer
 export function updateTimer(currentTime, lastBroadcast) {
-  // Get the time difference
   let timeDifference = getTimeDifference(currentTime, lastBroadcast)
-  let days = document.getElementById("days")
-  let hours = document.getElementById("hours")
-  let minutes = document.getElementById("minutes")
-  let seconds = document.getElementById("seconds")
+  let timeElements = {
+    days: document.getElementById("days"),
+    hours: document.getElementById("hours"),
+    minutes: document.getElementById("minutes"),
+    seconds: document.getElementById("seconds"),
+  }
+  let daysText = document.getElementById("days-text")
 
+  // Handle case where seconds is 60
   if (timeDifference.seconds === "60") {
     timeDifference.seconds = "00"
   }
 
-  // if NaN then set to 0
-  if (isNaN(timeDifference.days)) {
-    timeDifference.days = "."
-    //Add style
-    days.className = "bounce"
-  }
-  if (isNaN(timeDifference.hours)) {
-    timeDifference.hours = "."
-    hours.className = "bounce"
-  }
-  if (isNaN(timeDifference.minutes)) {
-    timeDifference.minutes = "."
-    minutes.className = "bounce"
-  }
-  if (isNaN(timeDifference.seconds)) {
-    timeDifference.seconds = "."
-    seconds.className = "bounce"
-  } else {
-    //Remove style
-    days.className = ""
-    hours.className = ""
-    minutes.className = ""
-    seconds.className = ""
-  }
+  // Handle case where time difference is NaN
+  Object.keys(timeDifference).forEach((key) => {
+    if (isNaN(timeDifference[key])) {
+      timeDifference[key] = "."
+      timeElements[key].className = "bounce"
+    } else {
+      timeElements[key].className = ""
+    }
+  })
 
-  // Change textcontent to "Day" if ends with 1
+  // Update days text
   if (timeDifference.days.endsWith("1") && !timeDifference.days.endsWith("11")) {
-    document.getElementById("days-text").textContent = "Day"
+    daysText.textContent = "Day"
   } else {
-    document.getElementById("days-text").textContent = "Days"
+    daysText.textContent = "Days"
   }
 
-  // Display
-  days.innerHTML = timeDifference.days
-  hours.innerHTML = timeDifference.hours
-  minutes.innerHTML = timeDifference.minutes
-  seconds.innerHTML = timeDifference.seconds
+  // Update time elements
+  Object.keys(timeElements).forEach((key) => {
+    timeElements[key].textContent = timeDifference[key]
+  })
 }
