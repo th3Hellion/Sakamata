31629d41e0e1e821fd3dedca3d55958c99079cdd
Nikita Grishin <ng222jk@student.lnu.se>
2023-02-04 15:36:40 +0100

    reorganized the project for better asset handling on build

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-export default {
-  root: "src",
-  assetsDir: "./src/img/",
-  build: {
-    base: "/Sakamata",
-    outDir: "../dist",
-    publicUrl: "./",
-    emptyOutDir: true,
-    target: "esnext",
-    polyfillDynamicImport: false,
-    files: ["index.html", "js/**/*.js", "css/**/*.css"],
-  },
-}
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  base: "./",
+})
